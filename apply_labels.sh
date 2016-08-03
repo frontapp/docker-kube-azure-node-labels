e4cc5a7ed6f44d388c6b1b61e181e63f3b44a585
Quentin Calvez <quentin.calvez@quentez.com>
2016-08-02 18:27:18 -0700

    :bug: Use the correct environment variable.

--- apply_labels.sh
+++ apply_labels.sh
@@ -7,12 +7,12 @@ FAULT_DOMAIN=`echo ${INSTANCE_INFO} | jq --raw-output '.FD'`
 # It appears it takes a while for the pod to incorporate the node name.
 while [ "x$NODE" = "x" ] || [ "$NODE" = "null" ]; do
   sleep 1
-  echo "[$(date)] Pod: $POD_NAME"
+  echo "[$(date)] Pod: $HOSTNAME"
   NODE=`curl  -s -f \
         --cert   /etc/kubernetes/ssl/worker.pem \
         --key    /etc/kubernetes/ssl/worker-key.pem \
         --cacert /etc/kubernetes/ssl/ca.pem  \
-        https://${KUBERNETES_SERVICE_HOST}/api/v1/namespaces/kube-system/pods/${POD_NAME} | jq -r '.spec.nodeName'
+        https://${KUBERNETES_SERVICE_HOST}/api/v1/namespaces/kube-system/pods/${HOSTNAME} | jq -r '.spec.nodeName'
   `
 done
 
